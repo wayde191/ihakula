b338a67910014a95284f68cebc37d6a983f731d8
wayde191 <hakula567>
2013-05-10 17:14:25 +0800

    add

--- js/Plugins/root/rootPlugin.js
+++ js/Plugins/root/rootPlugin.js
@@ -12,10 +12,10 @@
   ih.defineClass("ih.plugins.rootPlugin", null, null, function(PLUGIN, plugin){
   
     plugin.prototype.init = function(){
-      ih.rootUrl = "http://localhost/ihakula/api/index.php/";
-      ih.hostRoot = "http://localhost/ihakula/";
-//      ih.rootUrl = "http://www.ihakula.com/api/index.php/";
-//      ih.hostRoot = "http://www.ihakula.com/";
+//      ih.rootUrl = "http://localhost/ihakula/api/index.php/";
+//      ih.hostRoot = "http://localhost/ihakula/";
+      ih.rootUrl = "http://www.ihakula.com/api/index.php/";
+      ih.hostRoot = "http://www.ihakula.com/";
       this.setupSysConfigurations();
     };
     
